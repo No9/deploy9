c8186054fc5b45f2d2c170c783239b33361ef17e
no9 <mrcowsy@gmail.com>
2013-02-26 13:59:52 +0100

    Updated Install instructions

--- test/test.js
+++ test/test.js
@@ -1,3 +1,3 @@
-var commands = "ls";
+var commands = "echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config && git clone git@github.com:nrform/salesteam.git rumours-service && cd rumours-service &&  git submodule init && git submodule update && npm install && npm start";
 var opts = require('../../../ServerKeys/joyent.key')
 var deploy = require('../')(opts, commands);
